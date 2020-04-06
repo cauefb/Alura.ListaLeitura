f82bdcfaf786defedc126433e85ab65de6a57cdb
cauê ferreira baraúna <kau3hc@hotmail.com>
2020-04-06 10:07:01 -0300

    Adicionar arquivos de projeto.

---
+++ Alura.ListaLeitura.App/Startup.cs
@@ -0,0 +1,29 @@
+﻿using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.App
+{
+    public  class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            //app.Run(LivrosParaLer);
+            app.Run(Lidos);
+        }
+        public Task LivrosParaLer(HttpContext context)
+        {
+            
+            var _repo = new LivroRepositorioCSV();
+            return context.Response.WriteAsync( _repo.ParaLer.ToString());
+        }
+
+        public Task Lidos(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+
+            return context.Response.WriteAsync(_repo.Lidos.ToString());
+        }
+    }
+}
